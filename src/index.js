e9d21d9469536254a8b2d88ed7ad219421fb4a8e
Bianca Cristina Alves <biancacristinaalves@hotmail.com>
2021-07-23 19:42:59 -0300

    bolierplate

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ export const myFunction = () => {
   // aqui vai seu código
   console.log('Olá mundo!');
 };
+
+
+
+
+const email = 'teste@email.com';
+const password = '123456';
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log ('deu bom', user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log('deu ruim', errorCode, errorMessage)
+});
